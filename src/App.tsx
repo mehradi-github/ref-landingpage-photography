d2ad745148756f95f13753644f0a55d2fbd92856
Mehradi <mehradi.info@gmail.com>
2023-02-21 15:02:11 +0100

    Adding header and assets

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,27 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* header.main-head>nav>h1#logo{Joe Teailor}+ul>li*3>a[href=""]^^^div.burger>span.line$*3 */}
+      <header className="main-head">
+        <nav>
+          <h1 id="logo">Lorem, ipsum.</h1>
+          <ul className="nav-links">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#work">Work</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="burger">
+          <span className="line1"></span>
+          <span className="line2"></span>
+          <span className="line3"></span>
+        </div>
       </header>
     </div>
   );
