a80f290bd27f6365d9f3d8bdddfe9263c11a79f1
Mehradi <mehradi.info@gmail.com>
2023-02-23 19:20:36 +0100

    Adding html section hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import youtube from "./assets/img/youtube-symbol 1.svg";
 import instagram from "./assets/img/instagram 1.svg";
 import twitter from "./assets/img/twitter 1.svg";
 import "./App.scss";
+import youtube from "./assets/img/youtube-symbol 1.svg";
+import elenajoy from "./assets/img/elena-joy.png";
+import plant1 from "./assets/img/plant1.png";
+import plant2 from "./assets/img/plant2.png";
 
 function App() {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
@@ -42,7 +45,29 @@ function App() {
           </div>
         </nav>
       </header>
-      <main></main>
+      <main>
+        <section className="hero">
+          <div className="hero-introduction flex">
+            <h2>
+              Lorem, ipsum.
+              <br />
+              Photography
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              corporis laborum nihil! Assumenda repellat tempora, explicabo
+              accusamus debitis corrupti ab maiores modi minima, aliquam,
+              eligendi expedita deleniti a velit commodi adipisci non.
+            </p>
+            <a href="#gallery">Gallery</a>
+          </div>
+          <div className="hero-images">
+            <img className="hero-elena" src={elenajoy} alt="picture of lorem" />
+            <img src={plant1} alt="" className="plant1 plant" />
+            <img src={plant2} alt="" className="plant2 plant" />
+          </div>
+        </section>
+      </main>
       <footer>
         <h4> Lorem, ipsum. &copy; 2023</h4>
         <ul>
